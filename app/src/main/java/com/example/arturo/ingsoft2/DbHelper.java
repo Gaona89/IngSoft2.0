63e6bbaad2e7d7e36dbe875623db639da462f3c0
rubencabanhas <rubencabanhas.89@gmail.com>
2017-06-27 15:58:38 -0400

    Update DbHelper.java

--- app/src/main/java/com/example/arturo/ingsoft2/DbHelper.java
+++ app/src/main/java/com/example/arturo/ingsoft2/DbHelper.java
@@ -46,63 +46,7 @@ public class DbHelper extends SQLiteOpenHelper {
         db.execSQL(sqlCreateVacuna);
         db.execSQL(sqlCreateRegistroVacuna);
         db.execSQL(sqlCreateAplicacion);
-        /*Datos precargados*/
-        db.execSQL("INSERT INTO HIJO(ci,nombre,apellido,email,fecha_nac)"+
-                " VALUES(5555,'juan','perez','gaona.arturo89@gmail.com','2015-05-16')");
-
-        db.execSQL("INSERT INTO HIJO(ci,nombre,apellido,email,fecha_nac)"+
-                " VALUES(4444,'jose','perez','gaona.arturo89@gmail.com','2015-04-20')");
-
-        db.execSQL("INSERT INTO HIJO(ci,nombre,apellido,email,fecha_nac)"+
-                " VALUES(4444,'jose','fulano','gaona.arturo89@gmail.com','2016-03-12')");
-
-        db.execSQL("INSERT INTO registro_vacuna(id,ci_hijo,id_aplicacion,fecha_aplicacion)"+
-                " VALUES(1,5555,1,'2016-03-12')");
-
-        db.execSQL("insert into vacuna(id,nombre) values(1,'BCG')");
-        db.execSQL("insert into vacuna(id,nombre) values(2,'ROTAVIRUS')");
-        db.execSQL("insert into vacuna(id,nombre) values(3,'IPV/OPV')");
-        db.execSQL("insert into vacuna(id,nombre) values(4,'PENTAVALENTE')");
-        db.execSQL("insert into vacuna(id,nombre) values(5,'PCV10')");
-        db.execSQL("insert into vacuna(id,nombre) values(6,'SPR')");
-        db.execSQL("insert into vacuna(id,nombre) values(7,'ANTIVARICELA')");
-        db.execSQL("insert into vacuna(id,nombre) values(8,'HEPAT A')");
-        db.execSQL("insert into vacuna(id,nombre) values(9,'AA')");
-        db.execSQL("insert into vacuna(id,nombre) values(10,'DTP1')");
-        db.execSQL("insert into vacuna(id,nombre) values(11,'INFLUENZA')");
-        db.execSQL("insert into vacuna(id,nombre) values(12,'VPH')");
-        db.execSQL("insert into vacuna(id,nombre) values(13,'Tdpa')");
-
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(1,1,1,0)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(2,2,1,2)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(3,3,1,2)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(4,4,1,2)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(5,5,1,2)");
-
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(6,2,2,4)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(7,3,2,4)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(8,4,2,4)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(9,5,2,4)");
-
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(10,3,3,6)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(11,4,3,6)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(12,11,1,6)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(13,11,2,6)");
-
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(14,6,1,12)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(15,5,3,12)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(16,9,1,12)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(17,11,3,12)");
-
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(18,7,1,15)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(19,8,1,15)");
-
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(20,3,4,18)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(21,10,1,18)");
-
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(22,3,5,48)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(23,10,2,48)");
-        db.execSQL("insert into APLICACION(id,vacuna,dosis,tiempo_aplicacion) values(24,6,2,48)");
+       /*Datos precargados*/
     }
 
     @Override
